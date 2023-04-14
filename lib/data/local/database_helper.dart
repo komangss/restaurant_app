0c616de7776db5efcf7f60d65f5f193148c04e37
Antonio Komang Yudistira <komanggt12@gmail.com>
2023-04-14 23:31:49 +0800

    created database helper for favorite restaurant

---
+++ lib/data/local/database_helper.dart
@@ -0,0 +1,86 @@
+
+import 'package:sqflite/sqflite.dart';
+
+import '../../models/restaurant.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper? _instance;
+  static Database? _database;
+
+  DatabaseHelper._internal() {
+    _instance = this;
+  }
+
+  factory DatabaseHelper() => _instance ?? DatabaseHelper._internal();
+
+  static const String _tblRestaurant = 'restaurants';
+
+  Future<Database> _initializeDb() async {
+    var path = await getDatabasesPath();
+    var db = openDatabase(
+      '$path/newsapp.db',
+      onCreate: (db, version) async {
+        await db.execute('''CREATE TABLE $_tblRestaurant (
+             id TEXT PRIMARY KEY,
+             name TEXT,
+             description TEXT,
+             city TEXT,
+             address TEXT,
+             pictureId TEXT,
+             categories TEXT,
+             menus TEXT,
+             rating TEXT,
+             customerReviews TEXT,
+           )     
+        ''');
+      },
+      version: 1,
+    );
+
+    return db;
+  }
+
+  Future<Database?> get database async {
+    _database ??= await _initializeDb();
+
+    return _database;
+  }
+
+  Future<void> insertRestaurant(Restaurant restaurant) async {
+    final db = await database;
+    await db!.insert(_tblRestaurant, restaurant.toJson());
+  }
+
+  Future<List<Restaurant>> getRestaurants() async {
+    final db = await database;
+    List<Map<String, dynamic>> results = await db!.query(_tblRestaurant);
+
+    return results.map((res) => Restaurant.fromJson(res)).toList();
+  }
+
+  Future<Map> getRestaurantById(String id) async {
+    final db = await database;
+
+    List<Map<String, dynamic>> results = await db!.query(
+      _tblRestaurant,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (results.isNotEmpty) {
+      return results.first;
+    } else {
+      return {};
+    }
+  }
+
+  Future<void> removeRestaurant(String id) async {
+    final db = await database;
+
+    await db!.delete(
+      _tblRestaurant,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
